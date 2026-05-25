df64852a298b423b4e0a7fab8ddc02d7ae039705
beren87 <berenger.ferguenis@g5-digital.com>
2026-05-25 23:00:19 +0200

    [KOL-3] Architecture modulaire et création du composant Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
+import Footer from '@/components/layout/Footer';
+
 export default function Home() {
   return (
-    <main className='min-h-screen bg-slate-900 text-white flex flex-col items-center justify-center p-4'>
-      <h1 className='text-5xl font-bold text-yellow-500 mb-6 tracking-widest'>
-        KNIGHTS OF LIXXA
-      </h1>
-      <p className='text-xl text-gray-300'>
-        Bienvenue, Seigneur. Votre fief attend vos ordres.
-      </p>
+    <main className='min-h-screen bg-slate-900 text-white flex flex-col items-center justify-between p-6'>
+      <div /> {/* Espace du haut */}
+      {/* Le contenu central */}
+      <div className='text-center flex flex-col items-center justify-center'>
+        <h1 className='text-5xl font-bold text-yellow-500 mb-6 tracking-widest drop-shadow-[0_5px_5px_rgba(0,0,0,0.8)]'>
+          KNIGHTS OF LIXXA
+        </h1>
+        <p className='text-xl text-gray-300 italic max-w-md'>
+          {`"Bienvenue, Seigneur. Votre fief attend vos ordres."`}
+        </p>
+      </div>
+      {/* Notre nouvelle brique importée ! */}
+      <Footer />
     </main>
   );
 }
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className='text-sm text-gray-500 font-mono tracking-wider border-t border-slate-800 pt-4 w-full text-center mt-8'>
+      Alpha Version v0.1.0 • Knights of Lixxa
+    </footer>
+  );
+}
